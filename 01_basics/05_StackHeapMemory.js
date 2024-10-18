fd7e44e9702ca928221d3b7b6b4d00e3b7ee5ee3
nandini070702 <shrivastav.n2002@gmail.com>
2024-10-18 08:56:25 +0000

    stack and heap

---
+++ 01_basics/05_StackHeapMemory.js
@@ -0,0 +1,14 @@
+// STACK(Primitive), HEAP(Non-Primitive)
+
+let myYoutubename = "nandinishriv.com"
+
+let anothername = myYoutubename
+anothername = "chaiaucode"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com"
+}
+let userTwo = userOne
+userTwo.email = "nandini@google.com"
