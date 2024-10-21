aaa66a6cf5efe73337a019ad0b7cd4b72a055275
nandini070702 <shrivastav.n2002@gmail.com>
2024-10-21 10:20:56 +0000

    new changes

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,41 @@
+const name ="nandini"
+const repoCount = "23"
+
+//console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('nandini-hc')
+
+//console.log(gameNew[0]);
+//console.log(gameNew[0].__proto__);
+
+//console.log(gameNew.length);
+//console.log(gameNew.toLocaleUpperCase());
+//console.log(gameNew.charAt(5));
+//console.log(gameNew.indexOf('a'));
+
+const newString = gameName.substring(0, 4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-1, 3)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://nandini.com/nandini%20shriv"
+console.log(url.replace('%20', '39'));
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
